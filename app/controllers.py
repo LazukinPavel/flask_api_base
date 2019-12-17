15e0ef9965e352019ea00d462d0f3721c69c51ce
backend <backend@analitika.online>
2019-12-17 12:04:31 +0300

    Users endpoints updated with 'get_by_id' and 'create_user'.

--- app/controllers.py
+++ app/controllers.py
@@ -5,8 +5,27 @@ from playhouse.shortcuts import model_to_dict, dict_to_model
 
 
 class Controller:
+    def create_user(self, user_data):
+        user, created = User.get_or_create(
+            username=user_data['username'],
+            defaults=dict(
+                email=user_data['email']
+            )
+        )
+        if created:
+            return json.dumps(model_to_dict(user))
+        return f'User with name {user_data["username"]} already exists'
+
     def get_users(self):
-        users_qs = User.select().dicts()
+        users_qs = User.select(User.username, User.email).dicts()
         if users_qs:
             return json.dumps(list(users_qs))
         return 'No users in DB'
+
+    def get_users_by_id(self, user_id):
+        user = User.get_or_none(User.id == user_id)
+        if user:
+            return json.dumps(model_to_dict(user))
+        return 'User not found'     # TODO implement 404
+
+
--- app/views.py
+++ app/views.py
@@ -24,6 +24,28 @@ def root():
 
 @app.route('/users')
 def get_users():
-    data = controller.get_users()
+    users = controller.get_users()
+    return users
 
-    return data
+
+@app.route('/users/<user_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def get_user(user_id):
+
+    if request.method == 'GET':
+        result = controller.get_users_by_id(user_id)
+
+    elif request.method == 'POST' and user_id == 'new':
+        print('=== req', )
+        result = controller.create_user({
+            'username': request.args.get('username'),
+            'email': request.args.get('email')
+        })
+    else:
+        result = 'Unknown command, please refer API docs'
+
+    return result
+
+
+# @app.route('/users/create', methods=['POST'])
+# def create_user
+
